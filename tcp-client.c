ac42fe1e48c89f58b899918c2cba2f148937b2d2
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-02-13 11:39:39 +0100

    basic calculator functionality

--- tcp-client.c
+++ tcp-client.c
@@ -1,41 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
 #include <netinet/in.h>
 
+#define PORT 8080
+
+void server_calculator(int connection_fd) {
+	char buffer[256];
+	int n;
+	for(;;) {
+	
+		// print the text "skriv inn uttrykk" from the server 
+		bzero(buffer, 256);
+		printf("skriv inn uttrykk: \n");
+		n = 0;
+		while ((buffer[n++] = getchar()) != '\n')
+			;
+
+		// write the expression given by the user to the server 
+		// TODO do some validation and exception handling
+		write(connection_fd, buffer, sizeof(buffer));
+		bzero(buffer, 256);
+
+		// read the calculated value of the expresion from the server 
+		read(connection_fd, buffer, sizeof(buffer));
+		printf("=\n");
+		printf("%s\n", buffer);
+
+		if (strncmp(buffer, "exit", 4) == 0) {
+			printf("exiting...\n");
+			break;
+		}
+	}
+}
+
 int main() {
 
+	
 	// create a socket
 	int network_socket;
-	network_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	// specify an address for the socket
 	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
-	server_address.sin_addr.s_addr = INADDR_ANY; // sin_addr is a struct, accessing field s_addr to get the address of the server we are connecting to	
 	
-	// connect returns an integer: 0 = OK, -1 = ERROR
-	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-	// check for error with the connection 
-	if (connection_status == -1) {
-		printf("There was an error making a connection to the remote server \n\n");
+	network_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (network_socket == -1) {
+		printf("socket creation failed..\n");
+		exit(0);
+	}
+	else {
+		printf("socket created..\n");
 	}
 
-	// recive data from the server 
-	char server_response[256];
-	// put the data we recive from the socket in server_response. optional flags param is 0, left unused.
-	recv(network_socket, &server_response, sizeof(server_response), 0);
+	// specify an address for the socket
+	bzero(&server_address, sizeof(server_address));
 
-	//print out the server's response
-	printf("The server sent the data: %s\n", server_response);
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); // sin_addr is a struct, accessing field s_addr to get the address of the server we are connecting to	
+	
+	// connect returns an integer: 0 = OK, -1 = ERROR
+	if(connect(network_socket, 
+		(struct sockaddr*)&server_address, sizeof(server_address)) != 0) {
+		printf("connection with the server failed...");
+		exit(0);
+
+	}
+	else {
+		printf("connected to the server...\n");
+	}
+	 
+
+	// recive data from the server 
+	printf("-------------Calculator------------\n");	
+	server_calculator(network_socket);
 
 	// close the socket
 	close(network_socket);
-
+	
 	return 0;
 }
 
--- tcp-server.c
+++ tcp-server.c
@@ -1,40 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <strings.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 
 #include <netinet/in.h>
 
+#define PORT 8080
+
+int eval_expression(char* expression) {
+	printf("\nentred expression eval\n");
+
+	size_t length = strlen(expression);
+	int num1 = 0, num2 = 0;
+	char operator;
+
+	int i = 0;
+	// get first number of the expression 
+	while (expression[i] != '\0') {
+		if (expression[i] == '+' || expression[i] == '-') {
+			operator = expression[i];
+			i++;
+			break;
+		}
+		num1 = num1 * 10 + (expression[i] - '0');
+		i++;
+	}
+	printf("num 1: %d\n", num1);
+
+	printf("operator: %c\n", operator);
+	printf("num2: %d\n", num2);
+	// get second number of the expression 
+	while (expression[i] != '\n') {
+		num2 = num2 * 10 + (expression[i] - '0');
+		printf("expression : %c\n", expression[i]);
+		printf("num2: %d\n", num2);
+		i++;
+	}
+	
+	printf("exit expression eval\n");
+	if (operator == '+') return num1 + num2;
+	if (operator == '-') return num1 - num2;
+	return 0;	
+}
+
+void server_calculator(int connection_fd) {
+	printf("entred calculator\n");
+	char buffer[256];
+	
+	for(;;)
+	{
+		bzero(buffer, 256);
+
+		// reads the expression from the client 
+		read(connection_fd, buffer, sizeof(buffer));
+		printf("uttrykk fra klient: %s", buffer);
+		
+		// calulates expression 
+		int expression_value = eval_expression(buffer);
+		printf("%s = %d", buffer, expression_value);
+		
+		bzero(buffer, 256);
+		
+		sprintf(buffer, "%d", expression_value);	
+		// write expression and result to client 
+		write(connection_fd, buffer, sizeof(buffer));
+
+		if(strncmp("exit", buffer, 4) == 0) {
+			printf("exiting...\n");
+			break;
+		}
+	}
+}
+
 int main() {
 
-	char server_message[256] = "Du har annkommet serveren";
-
+	//TODO PROPER CLOSING, REUSE SAME PORT WITHOUT KILLLING PROCESS FROM TERMINAL	
 	// create the server socket 
-	int server_socket;
+	int server_socket, client_socket, length, option = 1;
+	struct sockaddr_in server_address, client_address;
+	
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket == -1) {
+		perror("socket creation failed..\n");
+		exit(EXIT_FAILURE);
+	} else {
+		printf("socket created..\n");
+	}
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
+	bzero(&server_address, sizeof(server_address));
 	// define the server address
-	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
-	server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_address.sin_port = htons(PORT);
 
 	// bind the socket to the specified IP and port 
-	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+	if ((bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)))
+			!= 0) {
+		perror("socket bind failed\n");
+		exit(EXIT_FAILURE);
+	} else {
+		printf("socket binded..\n");
+	}
 
 	// listen for connections. 5 specifies the total number of allowed connections at a time
-	listen(server_socket, 5);
-
-	// define client socket 
-	int client_socket;
-
-	while(1) {
-		client_socket = accept(server_socket, NULL, NULL);
-
-		// send data to the specified client 
-		send(client_socket, server_message, sizeof(server_message), 0);
+	if (listen(server_socket, 5) != 0) {
+		perror("listen failds..\n");
+		exit(EXIT_FAILURE);
+	} else {
+		printf("server listening..\n");
 	}
+	length = sizeof(client_address);
+	
+	// define client socket 
+	
+	client_socket = accept(
+		server_socket, (struct sockaddr*)&client_address, &length);
+	if (client_socket < 0) {
+		perror("server accept failed..\n");
+		exit(EXIT_FAILURE);
+	} else {
+		printf("server accept the client\n");
+	}
+	
+	server_calculator(client_socket);
+	printf("exited calculator. closing...\n");
 
 	close(server_socket);
 	return 0;
